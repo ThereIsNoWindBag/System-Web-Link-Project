940bb47a61c39fa4922bb7011f6c5c2867508a19
han <mgu1228@gmail.com>
2023-05-31 00:29:37 +0900

    feat add 5 sec timer in system_server.c

--- system/system_server.c
+++ system/system_server.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <sys/prctl.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
+int posix_sleep_ms(unsigned int timeout_ms)
+{
+    struct timespec sleep_time;
+
+    sleep_time.tv_sec = timeout_ms / MILLISEC_PER_SECOND;
+    sleep_time.tv_nsec = (timeout_ms % MILLISEC_PER_SECOND) * (NANOSEC_PER_USEC * USEC_PER_MILLISEC);
+
+    return nanosleep(&sleep_time, NULL);
+}
+
+void five_sec_alarm_handler(int sig)
+{
+    printf("%ld seconds\n", clock() / CLOCKS_PER_SEC);
+} 
+
 int system_server()
 {
+    struct itimerval itv;
+    struct sigaction sa;
+
     printf("나 system_server 프로세스!\n");
 
+    /* 5초 타이머를 만들어 봅시다. */
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = five_sec_alarm_handler;
+    sigaction(SIGALRM, &sa, NULL);
+    // signal(SIGALRM, five_sec_alarm_handler);
+
+    itv.it_value.tv_sec = 1; // 이걸 0으로 하면 문제가 발생한다. 왜일까?
+    itv.it_value.tv_usec = 0;
+    itv.it_interval.tv_sec = 5;
+    itv.it_interval.tv_usec = 0;
+
+    setitimer(ITIMER_REAL, &itv, NULL);
+
     while (1) {
-        sleep(1);
+        posix_sleep_ms(5000);
     }
 
     return 0;
