233323a1cef5c87e5b08c76215177cdda6b8f2a0
han <mgu1228@gmail.com>
2023-06-01 17:30:42 +0900

    feat mutex
    NOTE : this feature will be removed in the next commit.

--- ui/input.c
+++ ui/input.c
@@ -18,6 +18,7 @@
 
 #define TOY_TOK_BUFSIZE 64
 #define TOY_TOK_DELIM " \t\r\n\a"
+#define TOY_BUFFSIZE 1024
 
 typedef struct _sig_ucontext {
     unsigned long uc_flags;
@@ -26,6 +27,11 @@ typedef struct _sig_ucontext {
     struct sigcontext uc_mcontext;
     sigset_t uc_sigmask;
 } sig_ucontext_t;
+
+static pthread_mutex_t global_message_mutex  = PTHREAD_MUTEX_INITIALIZER;
+
+// global_message <~ 모든 문제를 만드는 전역 변수
+static char global_message[TOY_BUFFSIZE];
 
 void segfault_handler(int sig_num, siginfo_t * info, void * ucontext) {
   void * array[50];
@@ -72,8 +78,20 @@ void *sensor_thread(void* arg)
     printf("%s", s);
 
     while (1) {
+        i = 0;
+        // 여기서 뮤텍스
+        // 과제를 억지로 만들기 위해 한 글자씩 출력 후 슬립
+        pthread_mutex_lock(&global_message_mutex);
+        while (global_message[i] != NULL) {
+            printf("%c", global_message[i]);
+            fflush(stdout);
+            posix_sleep_ms(500);
+            i++;
+        }
+        pthread_mutex_unlock(&global_message_mutex);
         posix_sleep_ms(5000);
     }
+
 
     return 0;
 }
@@ -83,17 +101,20 @@ void *sensor_thread(void* arg)
  */
 
 int toy_send(char **args);
+int toy_mutex(char **args);
 int toy_shell(char **args);
 int toy_exit(char **args);
 
 char *builtin_str[] = {
     "send",
+    "mu",
     "sh",
     "exit"
 };
 
 int (*builtin_func[]) (char **) = {
     &toy_send,
+    &toy_mutex,
     &toy_shell,
     &toy_exit
 };
@@ -107,6 +128,20 @@ int toy_send(char **args)
 {
     printf("send message: %s\n", args[1]);
 
+    return 1;
+}
+
+int toy_mutex(char **args)
+{
+    if (args[1] == NULL) {
+        return 1;
+    }
+
+    printf("save message: %s\n", args[1]);
+    // 여기서 뮤텍스
+    pthread_mutex_lock(&global_message_mutex);
+    strcpy(global_message, args[1]);
+    pthread_mutex_unlock(&global_message_mutex);
     return 1;
 }
 
@@ -212,7 +247,9 @@ void toy_loop(void)
     int status;
 
     do {
+        pthread_mutex_lock(&global_message_mutex);
         printf("TOY>");
+        pthread_mutex_unlock(&global_message_mutex);
         line = toy_read_line();
         args = toy_split_line(line);
         status = toy_execute(args);
@@ -263,7 +300,7 @@ int input()
     pthread_t command_thread_tid, sensor_thread_tid;
     pthread_attr_t attr;
 
-      pthread_attr_init(&attr);
+    pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
     pthread_create(&command_thread_tid, &attr, command_thread, "command thread initiated");
